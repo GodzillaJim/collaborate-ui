5bc81cec08c1e67ada1b44a3b93da1b30b645c0c
GodzillaJim <jimnanjoroge76@gmail.com>
2022-09-09 13:56:56 +0300

    v1:deploying-v6 - socket uri

--- src/context/socket/index.tsx
+++ src/context/socket/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { io, Socket } from "socket.io-client";
-import { config } from "../../config";
 
-export const socket = io({
-  host: config.socket.host || "https://api.thejimna.com",
-  hostname: config.socket.host || "https://api.thejimna.com",
-  port: config.socket.port || 80,
-});
+export const socket = io(
+  "http://dev-env.eba-a2rv6tdz.us-east-1.elasticbeanstalk.com"
+);
 export const SocketContext = React.createContext<Socket>(socket);
