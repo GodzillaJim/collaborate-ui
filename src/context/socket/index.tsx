a736534d40f24fec87219d115518ec58e53bfaef
GodzillaJim <jimnanjoroge76@gmail.com>
2022-09-09 15:11:42 +0300

    v1:deploying-v7 - socket uri

--- src/context/socket/index.tsx
+++ src/context/socket/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { io, Socket } from "socket.io-client";
 
 export const socket = io(
-  "http://dev-env.eba-a2rv6tdz.us-east-1.elasticbeanstalk.com"
+  "https://dev-env.eba-a2rv6tdz.us-east-1.elasticbeanstalk.com"
 );
 export const SocketContext = React.createContext<Socket>(socket);
